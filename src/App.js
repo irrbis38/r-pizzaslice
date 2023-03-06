2f71376d9a983effd202698653e09e68d12bd042
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-03-06 15:53:42 +0300

    feat: add global styles and fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import "./App.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 export const App = () => {
-    return <div></div>;
+    return (
+        <>
+            <GlobalStyle />
+            <h1>Pizza Slice</h1>
+            <div>Hello</div>
+        </>
+    );
 };
